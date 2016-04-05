42b7a5007ef3e0864d723c85fd56fa04ce47fe76
Evangx Roger <roge_oz@hotmail.com>
2016-04-05 15:28:45 -0500

    File to read logs added

---
+++ lector.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+public class lector{
+
+File f;
+public void programa(){
+f=new File("C:\\Program Files\\Hearthstone\\Logs\\Zone.log");
+try{
+FileReader fr= new FileReader(f);
+Scanner sc= new Scanner(fr);
+int lineasprocesadas=0;
+while(sc.hasNextLine()){
+sc.nextLine();
+lineasprocesadas+=1;
+System.out.println(lineasprocesadas);
+}
+System.out.println(lineasprocesadas);
+}
+catch(Exception e){
+System.out.println(e.getMessage());
+}
+
+}
+
+public static void main(String [] args){
+lector l = new lector();
+l.programa();
+}
+}
